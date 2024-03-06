9a2d4f567caaf9088951146e10d12bf2c25c6770
Shivraj <shivrajofficial102@gmail.com>
2024-03-06 11:09:27 +0530

    Solve Word Break question from Neet Code problem

--- 1-DP-Problems/WordBreak.js
+++ 1-DP-Problems/WordBreak.js
@@ -2,6 +2,26 @@ function FindWordInDict(str, dict) {
     let len = str.length;
     const DP = Array(len).fill(false);
     for (let indx = 0; indx < len; indx++) {
+        for (let j = 0; j <= indx; j++) {
+            let tempstr = str.substring(j, indx + 1);
+            if (indx - j > 21) {
+                break;
+            }
+            const foundElement = dict.find((element) => element === tempstr);
+            if (foundElement !== undefined) {
+                if (DP[j - 1] || j === 0) {
+                    DP[indx] = true;
+                    break;
+                }
+            }
+        }
+    }
+    return DP[len - 1];
+}
+function FindWordInDictUtil(str, dict) {
+    let len = str.length;
+    const DP = Array(len).fill(false);
+    for (let indx = 0; indx < len; indx++) {
         for (let j = indx; j >= 0; j--) {
             let tempstr = str.substring(j, indx + 1);
             if (DP[indx] || indx - j > 21) {
@@ -17,15 +37,9 @@ function FindWordInDict(str, dict) {
             }
         }
     }
-
     return DP[len - 1];
 }
-
-const result = FindWordInDict("catsandog", [
-    "cats",
-    "dog",
-    "sand",
-    "and",
-    "cat",
-]);
+const result = FindWordInDict("applepenapple", ["apple", "pen"]);
+const result1 = FindWordInDictUtil("applepenapple", ["apple", "pen"]);
 console.log(result);
+console.log(result1);
